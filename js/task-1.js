220017d3b418c309ac65c84962035fdc636c946d
AlisonTorr <70812870+AlisonTorr@users.noreply.github.com>
2022-02-05 01:25:22 +0300

    Add files via upload

--- js/task-1.js
+++ js/task-1.js
@@ -1,10 +1,10 @@
 const name = "Генератор защитного поля";
 let price = 1000;
 
-let message = `Выбран «${name}», цена за штуку ${price} кредитов`;
+// let message = `Выбран «${name}», цена за штуку ${price} кредитов`;
 
 console.log(`Выбран «${name}», цена за штуку ${price} кредитов`);
 
-// price = 2000;
+price = 2000;
 
 console.log(`Выбран «${name}», цена за штуку ${price} кредитов`);
